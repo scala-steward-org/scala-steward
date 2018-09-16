0bff670ae560806262d3b4b5fe60fd348b5eb702
Frank S. Thomas <frank@timepit.eu>
2018-09-16 05:00:03 +0200

    Give sbt more memory

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+set -ex
+
 export PATH="/opt/jre/current/bin:$PATH"
 SCRIPT=$(readlink -f "$0")
 STEWARD_DIR=$(dirname "$SCRIPT")/..
 cd "$STEWARD_DIR"
 git pull
-export SBT_OPTS="-Xms256M -Xmx512M"
+export SBT_OPTS="-Xms256M -Xmx640M"
 sbt -no-colors ";clean ;core/assembly"
 JAR=$(find -name "*assembly*.jar" | head -n1)
 java -jar $JAR
