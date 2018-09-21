be11b97794885fbcc411b4d2deac606bd0b05fff
Frank S. Thomas <frank@timepit.eu>
2018-09-21 03:46:58 +0200

    Use http4s-blaze-client for forking instead of curl

---
+++ modules/core/src/main/scala/eu/timepit/scalasteward/gh/http4s/Http4sGitHubService.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2018 scala-steward contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.timepit.scalasteward.gh.http4s
+
+import cats.effect.{Resource, Sync}
+import cats.implicits._
+import eu.timepit.scalasteward.gh.http4s.Http4sGitHubService._
+import eu.timepit.scalasteward.gh.{AuthenticatedUser, GitHubRepo, GitHubRepoResponse, GitHubService}
+import org.http4s.Method.POST
+import org.http4s.circe.jsonOf
+import org.http4s.client.Client
+import org.http4s.headers.Authorization
+import org.http4s.{BasicCredentials, Headers, Request, Uri}
+
+class Http4sGitHubService[F[_]: Sync](client: Resource[F, Client[F]]) extends GitHubService[F] {
+  override def fork(user: AuthenticatedUser, repo: GitHubRepo): F[GitHubRepoResponse] = {
+    val url = s"https://api.github.com/repos/${repo.owner}/${repo.repo}/forks"
+    Sync[F].fromEither(Uri.fromString(url)).flatMap { uri =>
+      val req = Request[F](POST, uri, headers = Headers(toBasicAuth(user)))
+      client.use(_.expect[GitHubRepoResponse](req)(jsonOf))
+    }
+  }
+}
+
+object Http4sGitHubService {
+  def toBasicAuth(user: AuthenticatedUser): Authorization =
+    Authorization(BasicCredentials(user.login, user.accessToken))
+}
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,6 +5,8 @@ object Dependencies {
   val catsEffect = "org.typelevel" %% "cats-effect" % "1.0.0"
   val circeParser = "io.circe" %% "circe-parser" % "0.10.0-M2"
   val fs2Core = "co.fs2" %% "fs2-core" % "1.0.0-M5"
+  val http4sBlazeClient = "org.http4s" %% "http4s-blaze-client" % Versions.http4s
+  val http4sCirce = "org.http4s" %% "http4s-circe" % Versions.http4s
   val refined = "eu.timepit" %% "refined" % "0.9.2"
   val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
 }
