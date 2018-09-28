5a84c0959c4b8c740dd0aefbe266a25d893d3be4
Frank S. Thomas <frank@timepit.eu>
2018-09-28 23:08:19 +0200

    Increase max memory

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,7 +7,7 @@ SCRIPT=$(readlink -f "$0")
 STEWARD_DIR=$(dirname "$SCRIPT")/..
 cd "$STEWARD_DIR"
 git pull
-export SBT_OPTS="-Xms256M -Xmx640M"
+export SBT_OPTS="-Xms256M -Xmx768M"
 sbt -no-colors ";clean ;core/assembly"
 JAR=$(find -name "*assembly*.jar" | head -n1)
 java -jar $JAR
