b25e031bbc9707dce0e19d839978d054d22fb853
Frank S. Thomas <frank@timepit.eu>
2018-10-04 22:23:18 +0200

    Test ProcessAlg

---
+++ modules/core/src/test/scala/eu/timepit/scalasteward/io/ProcessAlgTest.scala
@@ -0,0 +1,20 @@
+package eu.timepit.scalasteward.io
+
+import better.files.File
+import cats.effect.IO
+import org.scalatest.{FunSuite, Matchers}
+
+class ProcessAlgTest extends FunSuite with Matchers {
+  val processAlg: ProcessAlg[IO] =
+    ProcessAlg.sync[IO]
+
+  test("exec echo") {
+    processAlg
+      .exec(List("echo", "hello"), File.currentWorkingDirectory)
+      .unsafeRunSync() shouldBe List("hello")
+  }
+
+  test("exec false") {
+    processAlg.exec(List("false"), File.currentWorkingDirectory).attempt.unsafeRunSync().isLeft
+  }
+}
