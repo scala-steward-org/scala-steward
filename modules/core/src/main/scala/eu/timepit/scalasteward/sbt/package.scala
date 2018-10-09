b9342f79bb789df21f12565d9721fe4f6dc4a414
Frank S. Thomas <frank@timepit.eu>
2018-10-09 07:15:21 +0200

    Update default sbt version

--- modules/core/src/main/scala/eu/timepit/scalasteward/sbt/package.scala
+++ modules/core/src/main/scala/eu/timepit/scalasteward/sbt/package.scala
@@ -22,7 +22,7 @@ import scala.io.Source
 
 package object sbt {
   val defaultSbtVersion: SbtVersion =
-    SbtVersion("1.2.3")
+    SbtVersion("1.2.4")
 
   def seriesToSpecificVersion(sbtSeries: SbtVersion): SbtVersion =
     sbtSeries.value match {
