1e675bf166c9eff5d40d99754236970fc732cda6
Frank S. Thomas <frank@timepit.eu>
2018-10-15 21:35:56 +0200

    Add divideOnError which will be used later for checking for updates

--- modules/core/src/main/scala/eu/timepit/scalasteward/util/package.scala
+++ modules/core/src/main/scala/eu/timepit/scalasteward/util/package.scala
@@ -16,13 +16,27 @@
 
 package eu.timepit.scalasteward
 
+import cats.data.NonEmptyList
 import cats.implicits._
-import cats.{ApplicativeError, Monad, MonadError}
+import cats.{ApplicativeError, Foldable, Monad, MonadError, Semigroup}
 
 package object util {
   type ApplicativeThrowable[F[_]] = ApplicativeError[F, Throwable]
 
   type MonadThrowable[F[_]] = MonadError[F, Throwable]
+
+  def divideOnError[F[_], G[_], A, B, E](divide: A => G[A])(f: A => F[B])(a: A)(
+      implicit
+      F: ApplicativeError[F, E],
+      G: Foldable[G],
+      B: Semigroup[B]
+  ): F[B] =
+    f(a).handleErrorWith { e =>
+      NonEmptyList.fromFoldable(divide(a)) match {
+        case None      => F.raiseError(e)
+        case Some(nel) => nel.traverse(divideOnError(divide)(f)(_)).map(_.reduce)
+      }
+    }
 
   def ifTrue[F[_]](fb: F[Boolean])(f: F[Unit])(implicit F: Monad[F]): F[Unit] =
     fb.ifM(f, F.unit)
---
+++ modules/core/src/test/scala/eu/timepit/scalasteward/util/utilTest.scala
@@ -0,0 +1,19 @@
+package eu.timepit.scalasteward.util
+
+import cats.implicits._
+import org.scalatest.{FunSuite, Matchers}
+
+class utilTest extends FunSuite with Matchers {
+  test("divideOnError") {
+    /*
+       5
+    => (4, 3)
+    => ((3, 2), (2, 1))
+    => (((2, 1), (1, 0)), ((1, 0), 1))
+    => ((((1, 0), 1), (1, 0)), ((1, 0), 1))
+     */
+    divideOnError((i: Int) => if (i > 1) List(i - 1, i - 2) else Nil)(
+      i => if (i <= 1) i.asRight[Unit] else ().asLeft[Int]
+    )(5) shouldBe Right(5)
+  }
+}
