8a2d9c06695b78c6761e3ab27e3cb348ec8844e5
Frank S. Thomas <frank@timepit.eu>
2018-10-19 20:37:57 +0200

    Use Nel instead of List

--- modules/core/src/main/scala/eu/timepit/scalasteward/git/GitAlg.scala
+++ modules/core/src/main/scala/eu/timepit/scalasteward/git/GitAlg.scala
@@ -17,6 +17,7 @@
 package eu.timepit.scalasteward.git
 
 import better.files.File
+import cats.data.{NonEmptyList => Nel}
 import cats.effect.Sync
 import cats.implicits._
 import eu.timepit.scalasteward.github.data.Repo
@@ -70,65 +71,65 @@ object GitAlg {
     new GitAlg[F] {
       override def branchAuthors(repo: Repo, branch: Branch, base: Branch): F[List[String]] =
         workspaceAlg.repoDir(repo).flatMap { repoDir =>
-          exec(List("log", "--pretty=format:'%an'", dotdot(base, branch)), repoDir)
+          exec(Nel.of("log", "--pretty=format:'%an'", dotdot(base, branch)), repoDir)
         }
 
       override def checkoutBranch(repo: Repo, branch: Branch): F[Unit] =
         for {
           repoDir <- workspaceAlg.repoDir(repo)
-          _ <- exec(List("checkout", branch.name), repoDir)
+          _ <- exec(Nel.of("checkout", branch.name), repoDir)
         } yield ()
 
       override def clone(repo: Repo, url: Uri): F[Unit] =
         for {
           rootDir <- workspaceAlg.rootDir
           repoDir <- workspaceAlg.repoDir(repo)
-          _ <- exec(List("clone", url.toString, repoDir.pathAsString), rootDir)
+          _ <- exec(Nel.of("clone", url.toString, repoDir.pathAsString), rootDir)
         } yield ()
 
       override def commitAll(repo: Repo, message: String): F[Unit] =
         for {
           repoDir <- workspaceAlg.repoDir(repo)
-          _ <- exec(List("commit", "--all", "-m", message), repoDir)
+          _ <- exec(Nel.of("commit", "--all", "-m", message), repoDir)
         } yield ()
 
       override def containsChanges(repo: Repo): F[Boolean] =
         workspaceAlg.repoDir(repo).flatMap { repoDir =>
-          exec(List("status", "--porcelain"), repoDir).map(_.nonEmpty)
+          exec(Nel.of("status", "--porcelain"), repoDir).map(_.nonEmpty)
         }
 
       override def createBranch(repo: Repo, branch: Branch): F[Unit] =
         for {
           repoDir <- workspaceAlg.repoDir(repo)
-          _ <- exec(List("checkout", "-b", branch.name), repoDir)
+          _ <- exec(Nel.of("checkout", "-b", branch.name), repoDir)
         } yield ()
 
       override def currentBranch(repo: Repo): F[Branch] =
         for {
           repoDir <- workspaceAlg.repoDir(repo)
-          lines <- exec(List("rev-parse", "--abbrev-ref", "HEAD"), repoDir)
+          lines <- exec(Nel.of("rev-parse", "--abbrev-ref", "HEAD"), repoDir)
         } yield Branch(lines.mkString.trim)
 
       override def isBehind(repo: Repo, branch: Branch, base: Branch): F[Boolean] =
         workspaceAlg.repoDir(repo).flatMap { repoDir =>
-          exec(List("log", "--pretty=format:'%h'", dotdot(branch, base)), repoDir).map(_.nonEmpty)
+          exec(Nel.of("log", "--pretty=format:'%h'", dotdot(branch, base)), repoDir).map(_.nonEmpty)
         }
 
       override def isMerged(repo: Repo, branch: Branch, base: Branch): F[Boolean] =
         workspaceAlg.repoDir(repo).flatMap { repoDir =>
-          exec(List("log", "--pretty=format:'%h'", dotdot(base, branch)), repoDir).map(_.isEmpty)
+          exec(Nel.of("log", "--pretty=format:'%h'", dotdot(base, branch)), repoDir).map(_.isEmpty)
         }
 
       override def push(repo: Repo, branch: Branch): F[Unit] =
         for {
           repoDir <- workspaceAlg.repoDir(repo)
-          _ <- exec(List("push", "--force", "--set-upstream", "origin", branch.name), repoDir)
+          _ <- exec(Nel.of("push", "--force", "--set-upstream", "origin", branch.name), repoDir)
         } yield ()
 
       override def remoteBranchExists(repo: Repo, branch: Branch): F[Boolean] =
         for {
           repoDir <- workspaceAlg.repoDir(repo)
-          branches <- exec(List("branch", "-r"), repoDir)
+          branches <- exec(Nel.of("branch", "-r"), repoDir)
         } yield branches.exists(_.endsWith(branch.name))
 
       override def removeClone(repo: Repo): F[Unit] =
@@ -137,14 +138,14 @@ object GitAlg {
       override def resetHard(repo: Repo, base: Branch): F[Unit] =
         for {
           repoDir <- workspaceAlg.repoDir(repo)
-          _ <- exec(List("reset", "--hard", base.name), repoDir)
+          _ <- exec(Nel.of("reset", "--hard", base.name), repoDir)
         } yield ()
 
       override def setAuthor(repo: Repo, author: Author): F[Unit] =
         for {
           repoDir <- workspaceAlg.repoDir(repo)
-          _ <- exec(List("config", "user.email", author.email), repoDir)
-          _ <- exec(List("config", "user.name", author.name), repoDir)
+          _ <- exec(Nel.of("config", "user.email", author.email), repoDir)
+          _ <- exec(Nel.of("config", "user.name", author.name), repoDir)
         } yield ()
 
       override def syncFork(repo: Repo, upstreamUrl: Uri, defaultBranch: Branch): F[Unit] =
@@ -153,14 +154,14 @@ object GitAlg {
           remote = "upstream"
           branch = defaultBranch.name
           remoteBranch = s"$remote/$branch"
-          _ <- exec(List("remote", "add", remote, upstreamUrl.toString), repoDir)
-          _ <- exec(List("fetch", remote), repoDir)
-          _ <- exec(List("checkout", "-B", branch, "--track", remoteBranch), repoDir)
-          _ <- exec(List("merge", remoteBranch), repoDir)
+          _ <- exec(Nel.of("remote", "add", remote, upstreamUrl.toString), repoDir)
+          _ <- exec(Nel.of("fetch", remote), repoDir)
+          _ <- exec(Nel.of("checkout", "-B", branch, "--track", remoteBranch), repoDir)
+          _ <- exec(Nel.of("merge", remoteBranch), repoDir)
           _ <- push(repo, defaultBranch)
         } yield ()
 
-      def exec(command: List[String], cwd: File): F[List[String]] =
-        processAlg.exec(gitCmd :: command, cwd)
+      def exec(command: Nel[String], cwd: File): F[List[String]] =
+        processAlg.exec(gitCmd :: command.toList, cwd)
     }
 }
