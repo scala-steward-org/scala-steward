aa1474212ffdd89cc6069b1dd235b6d0d1293327
Frank S. Thomas <frank@timepit.eu>
2019-01-21 22:51:03 +0100

    Add test that discards an update

--- modules/core/src/test/scala/org/scalasteward/core/update/FilterAlgTest.scala
+++ modules/core/src/test/scala/org/scalasteward/core/update/FilterAlgTest.scala
@@ -14,4 +14,9 @@ class FilterAlgTest extends FunSuite with Matchers {
     val update = Update.Single("com.jsuereth", "sbt-pgp", "1.1.2-1", Nel.of("1.1.2", "2.0.0"))
     FilterAlg.removeBadVersions(update) shouldBe Some(update.copy(newerVersions = Nel.of("2.0.0")))
   }
+
+  test("removeBadVersions: update with only bad versions") {
+    val update = Update.Single("org.http4s", "http4s-dsl", "0.18.0", Nel.of("0.19.0"))
+    FilterAlg.removeBadVersions(update) shouldBe None
+  }
 }
