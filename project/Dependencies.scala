d8b630752a177b1fcf78c2431ae8134d22728d8c
Scala steward <me@scala-steward.org>
2019-02-18 02:07:22 +0100

    Update log4cats-slf4j to 0.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
   val http4sBlazeClient = "org.http4s" %% "http4s-blaze-client" % Versions.http4s
   val http4sCirce = "org.http4s" %% "http4s-circe" % Versions.http4s
   val kindProjector = "org.spire-math" %% "kind-projector" % "0.9.9"
-  val log4catsSlf4j = "io.chrisdavenport" %% "log4cats-slf4j" % "0.2.0"
+  val log4catsSlf4j = "io.chrisdavenport" %% "log4cats-slf4j" % "0.3.0"
   val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.2.3"
   val refined = "eu.timepit" %% "refined" % Versions.refined
   val refinedScalacheck = "eu.timepit" %% "refined-scalacheck" % Versions.refined
