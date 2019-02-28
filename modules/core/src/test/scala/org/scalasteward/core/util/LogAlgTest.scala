3930b563296af80dfb9c6409bce5b0aab0d05853
Frank S. Thomas <frank@timepit.eu>
2019-02-28 20:25:16 +0100

    Test attemptLog_

--- modules/core/src/test/scala/org/scalasteward/core/util/LogAlgTest.scala
+++ modules/core/src/test/scala/org/scalasteward/core/util/LogAlgTest.scala
@@ -1,10 +1,20 @@
 package org.scalasteward.core.util
 
+import cats.effect.Sync
 import org.scalasteward.core.mock.MockContext.{logAlg, logger => logger1}
-import org.scalasteward.core.mock.MockState
+import org.scalasteward.core.mock.{MockEff, MockState}
 import org.scalatest.{FunSuite, Matchers}
 
 class LogAlgTest extends FunSuite with Matchers {
+  test("attemptLog_") {
+    case class Err(msg: String) extends Throwable(msg)
+    val err = Err("hmm?")
+    val state =
+      logAlg.attemptLog_("run")(Sync[MockEff].raiseError(err)).runS(MockState.empty).unsafeRunSync()
+
+    state.logs shouldBe Vector((None, "run"), (Some(err), "run failed"))
+  }
+
   test("infoTimed") {
     val state = logAlg
       .infoTimed(_ => "timed")(logger1.info("inner"))
