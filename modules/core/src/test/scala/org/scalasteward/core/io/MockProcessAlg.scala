baac5b7387bf6947b57bbc66b38a3fe23747b915
Michael Wizner <mwz@users.noreply.github.com>
2019-03-10 21:40:43 +0000

    scalafmt

--- modules/core/src/test/scala/org/scalasteward/core/io/MockProcessAlg.scala
+++ modules/core/src/test/scala/org/scalasteward/core/io/MockProcessAlg.scala
@@ -2,7 +2,7 @@ package org.scalasteward.core.io
 
 import better.files.File
 import org.scalasteward.core.application.Config
-import org.scalasteward.core.mock.{MockEff, applyPure}
+import org.scalasteward.core.mock.{applyPure, MockEff}
 import org.scalasteward.core.util.Nel
 
 class MockProcessAlg(implicit config: Config) extends ProcessAlg.UsingFirejail[MockEff](config) {
