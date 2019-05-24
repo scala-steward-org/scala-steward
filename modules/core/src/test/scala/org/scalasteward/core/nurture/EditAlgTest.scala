788221301d6a7134bac4433da3983e73e86ac457
Frank S. Thomas <frank@timepit.eu>
2019-05-24 18:46:15 +0200

    Reproduce failed typesafe:config update

--- modules/core/src/test/scala/org/scalasteward/core/nurture/EditAlgTest.scala
+++ modules/core/src/test/scala/org/scalasteward/core/nurture/EditAlgTest.scala
@@ -33,4 +33,28 @@ class EditAlgTest extends FunSuite with Matchers {
       files = Map(file -> """val catsVersion = "1.3.0"""")
     )
   }
+
+  def runApplyUpdate(update: Update, files: Map[String, String]): Map[String, String] = {
+    val repoDir = File.temp / "ws/owner/repo"
+    val filesInRepoDir = files.map { case (file, content) => repoDir / file -> content }
+    editAlg
+      .applyUpdate(Repo("owner", "repo"), update)
+      .runS(MockState.empty.addFiles(filesInRepoDir))
+      .map(_.files)
+      .unsafeRunSync()
+      .map { case (file, content) => file.toString.replace(repoDir.toString + "/", "") -> content }
+  }
+
+  test("reproduce https://github.com/circe/circe-config/pull/40") {
+    val update = Update.Single("com.typesafe", "config", "1.3.3", Nel.of("1.3.4"))
+    val original = Map(
+      "build.sbt" -> """val config = "1.3.3"""",
+      "project/plugins.sbt" -> """addSbtPlugin("com.typesafe.sbt" % "sbt-site" % "1.3.3")"""
+    )
+    val expected = Map(
+      "build.sbt" -> """val config = "1.3.3"""", // the version should have been updated here
+      "project/plugins.sbt" -> """addSbtPlugin("com.typesafe.sbt" % "sbt-site" % "1.3.4")"""
+    )
+    runApplyUpdate(update, original) shouldBe expected
+  }
 }
