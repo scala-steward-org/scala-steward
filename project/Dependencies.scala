4fc644af16a764ff0e829417676cbe9d816fec40
Scala Steward <me@scala-steward.org>
2019-06-13 10:29:03 +0200

    Update monocle-core to 1.6.0-RC1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -25,7 +25,7 @@ object Dependencies {
   val kindProjector = "org.typelevel" %% "kind-projector" % "0.10.3"
   val log4catsSlf4j = "io.chrisdavenport" %% "log4cats-slf4j" % "0.3.0"
   val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.2.3"
-  val monocleCore = "com.github.julien-truffaut" %% "monocle-core" % "1.5.1-cats"
+  val monocleCore = "com.github.julien-truffaut" %% "monocle-core" % "1.6.0-RC1"
   val refined = "eu.timepit" %% "refined" % Versions.refined
   val refinedCats = "eu.timepit" %% "refined-cats" % Versions.refined
   val refinedScalacheck = "eu.timepit" %% "refined-scalacheck" % Versions.refined
