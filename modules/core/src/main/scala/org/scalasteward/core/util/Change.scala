237616add91b28e2ff7fa6a2a3e4a542f98ed6ad
Frank S. Thomas <frank@timepit.eu>
2019-07-16 20:12:16 +0200

    Document Change

--- modules/core/src/main/scala/org/scalasteward/core/util/Change.scala
+++ modules/core/src/main/scala/org/scalasteward/core/util/Change.scala
@@ -19,10 +19,19 @@ package org.scalasteward.core.util
 import cats.{Eq, Monoid}
 import org.scalasteward.core.util.Change.Changed
 
-sealed trait Change[T] {
+/** Data type that indicates whether some value of type `T` has been changed
+  * or not via its two constructors `Changed` and `Unchanged`.
+  *
+  * It is isomorphic to `(T, Boolean)`.
+  *
+  * `Change[T]` is a monoid for any monoid `T` with `Unchanged(T.empty)` as
+  * empty element and `Changed` combined with any other `Change` results in a
+  * `Changed` of the combined `T` values.
+  */
+sealed trait Change[T] extends Product with Serializable {
   def value: T
 
-  def someIfChanged: Option[T] =
+  final def someIfChanged: Option[T] =
     this match {
       case Changed(t) => Some(t)
       case _          => None
