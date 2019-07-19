29860e5072498529ec6904d4adcc934a03a3acc0
exoego <mogami@exoego.net>
2019-07-20 06:25:14 +0900

    Remove unnecessary code since UpdateHeuristic is not used for scala

--- modules/core/src/main/scala/org/scalasteward/core/io/package.scala
+++ modules/core/src/main/scala/org/scalasteward/core/io/package.scala
@@ -25,15 +25,13 @@ package object io {
     val scalaOrSbtFile = file.extension.exists(Set(".scala", ".sbt"))
     val travisYmlFile = file.name === ".travis.yml"
     val sbtPropertiesFile = file.name === "build.properties"
-    val scalafmtConfigFile = file.name === ".scalafmt.conf"
     val notInGitDir = !file.pathAsString.contains(".git/")
-    (scalaOrSbtFile || travisYmlFile || sbtPropertiesFile || scalafmtConfigFile) && notInGitDir
+    (scalaOrSbtFile || travisYmlFile || sbtPropertiesFile) && notInGitDir
   }
 
   def isFileSpecificTo(update: Update)(f: File): Boolean =
     update match {
-      case Update.Single("org.scala-sbt", "sbt", _, _, _)      => f.name === "build.properties"
-      case Update.Single("org.scalameta", "scalafmt", _, _, _) => f.name === ".scalafmt.conf"
-      case _                                                   => true
+      case Update.Single("org.scala-sbt", "sbt", _, _, _) => f.name === "build.properties"
+      case _                                              => true
     }
 }
