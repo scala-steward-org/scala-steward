06048e7c9fb6c4b85ce0bee78afd084fb1501afa
Frank S. Thomas <frank@timepit.eu>
2019-09-21 21:28:06 +0200

    Move repos.md to scala-steward-org/repos

--- scripts/run.sh
+++ scripts/run.sh
@@ -11,6 +11,8 @@ git pull
 sbt -no-colors ";clean ;core/assembly"
 JAR=$(find modules/ -name "scala-steward-assembly*.jar" | head -n1)
 
+curl -O https://raw.githubusercontent.com/scala-steward-org/repos/master/repos.md
+
 LOGIN="scala-steward"
 java -DROOT_LOG_LEVEL=INFO -DLOG_LEVEL=INFO -jar ${JAR} \
   --workspace  "$STEWARD_DIR/workspace" \
