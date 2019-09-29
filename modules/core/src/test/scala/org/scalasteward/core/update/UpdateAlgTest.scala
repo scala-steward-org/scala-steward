12fce13354021793f88a8d44f2485332e579310f
Frank S. Thomas <frank@timepit.eu>
2019-09-29 21:12:01 +0200

    Simple test of dependencyToUpdate

--- modules/core/src/test/scala/org/scalasteward/core/update/UpdateAlgTest.scala
+++ modules/core/src/test/scala/org/scalasteward/core/update/UpdateAlgTest.scala
@@ -7,12 +7,12 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class UpdateAlgTest extends AnyFunSuite with Matchers {
   test("findUpdateUnderNewGroup: returns empty if dep is not listed") {
-    val original = new Dependency("org.spire-math", "UNKNOWN", List("_2.12"), "1.0.0")
+    val original = Dependency("org.spire-math", "UNKNOWN", List("_2.12"), "1.0.0")
     UpdateAlg.findUpdateUnderNewGroup(original) shouldBe None
   }
 
-  test("findUpdateUnderNewGroup: returns Update.Single for updateing groupId") {
-    val original = new Dependency("org.spire-math", "kind-projector", List("_2.12"), "0.9.0")
+  test("findUpdateUnderNewGroup: returns Update.Single for updating groupId") {
+    val original = Dependency("org.spire-math", "kind-projector", List("_2.12"), "0.9.0")
     UpdateAlg.findUpdateUnderNewGroup(original) shouldBe Some(
       Update.Single(
         "org.spire-math",
@@ -21,6 +21,13 @@ class UpdateAlgTest extends AnyFunSuite with Matchers {
         Nel.of("0.10.0"),
         newerGroupId = Some("org.typelevel")
       )
+    )
+  }
+
+  test("dependencyToUpdate") {
+    val dependency = Dependency("groupId", "artifactId", List.empty, "1", Some(List("2")))
+    UpdateAlg.dependencyToUpdate(dependency) shouldBe Some(
+      Update.Single("groupId", "artifactId", "1", Nel.of("2"))
     )
   }
 }
