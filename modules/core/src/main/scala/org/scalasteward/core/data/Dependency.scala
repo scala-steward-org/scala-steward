3e7fc004dfc2d4aa14a69ec1cbc0d5379fc45634
Frank S. Thomas <f.thomas@gmx.de>
2019-10-16 14:23:23 +0200

    Resolve POMs of sbt plugins (#1044)
    This adds `sbtVersion` and `scalaVersion` attributes to
    `coursier.Dependency` to correctly resolve POMs of sbt plugins. With
    this change, Scala Steward shows the SCM URL or homepage in the PR body
    of sbt plugins that are on Maven Central.
    
    Here is a PR of sbt-sonatype before this change:
    https://github.com/fthomas/refined-sjs-example/pull/57
    
    and here with this change:
    https://github.com/fthomas/refined-sjs-example/pull/63

--- modules/core/src/main/scala/org/scalasteward/core/data/Dependency.scala
+++ modules/core/src/main/scala/org/scalasteward/core/data/Dependency.scala
@@ -18,7 +18,7 @@ package org.scalasteward.core.data
 
 import io.circe.generic.semiauto._
 import io.circe.{Decoder, Encoder}
-import org.scalasteward.core.sbt.data.SbtVersion
+import org.scalasteward.core.sbt.data.{SbtVersion, ScalaVersion}
 import org.scalasteward.core.util.Nel
 
 final case class Dependency(
@@ -27,6 +27,7 @@ final case class Dependency(
     artifactIdCross: String,
     version: String,
     sbtVersion: Option[SbtVersion] = None,
+    scalaVersion: Option[ScalaVersion] = None,
     configurations: Option[String] = None
 ) {
   def formatAsModuleId: String =
