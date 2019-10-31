109b67174d3d16bdd7e8c1a1670612c1ea4d03e7
Scala Steward <me@scala-steward.org>
2019-10-31 23:16:10 +0100

    Update sbt-scalafix to 0.9.8

--- modules/core/src/main/scala/org/scalasteward/core/sbt/package.scala
+++ modules/core/src/main/scala/org/scalasteward/core/sbt/package.scala
@@ -59,7 +59,7 @@ package object sbt {
   val scalaStewardScalafixSbt: FileData =
     FileData(
       "scala-steward-scalafix.sbt",
-      """addSbtPlugin("ch.epfl.scala" % "sbt-scalafix" % "0.9.7")"""
+      """addSbtPlugin("ch.epfl.scala" % "sbt-scalafix" % "0.9.8")"""
     )
 
   val stewardPlugin: FileData = {
