77a952d9d9d289dacee27770a051b40c10e2b6d6
Frank S. Thomas <frank@timepit.eu>
2019-11-06 21:33:04 +0100

    Add in-memory cache to HttpExistenceClient
    This adds an in-memory cache to HttpExistenceClient which caches the
    status codes of HEAD requests it does. The effect of this cache is that
    repeated calls to `VCSExtraAlg.getReleaseNoteUrl` for example now only
    take a few milliseconds:
    
    ```scala
    scala> dateTimeAlg.timed(vcsExtraAlg.getReleaseNoteUrl("https://github.com/fthomas/refined", data.Update.Single(data.GroupId("eu.timepit"), "refined", "0.9.9", Nel.of("0.9.10")))).unsafeRunSync
    res0: (Option[String], scala.concurrent.duration.FiniteDuration) =
      (Some(https://github.com/fthomas/refined/releases/tag/v0.9.10),7997 milliseconds)
    
    scala> dateTimeAlg.timed(vcsExtraAlg.getReleaseNoteUrl("https://github.com/fthomas/refined", data.Update.Single(data.GroupId("eu.timepit"), "refined", "0.9.9", Nel.of("0.9.10")))).unsafeRunSync
    res1: (Option[String], scala.concurrent.duration.FiniteDuration) =
      (Some(https://github.com/fthomas/refined/releases/tag/v0.9.10),8 milliseconds)
    ```
    
    Closes #941.

--- modules/core/src/main/scala/org/scalasteward/core/util/HttpExistenceClient.scala
+++ modules/core/src/main/scala/org/scalasteward/core/util/HttpExistenceClient.scala
@@ -16,23 +16,46 @@
 
 package org.scalasteward.core.util
 
+import cats.effect.{Async, Resource}
 import cats.implicits._
+import com.github.benmanes.caffeine.cache.Caffeine
 import io.chrisdavenport.log4cats.Logger
 import org.http4s.client.Client
 import org.http4s.{Method, Request, Status, Uri}
+import scalacache.CatsEffect.modes._
+import scalacache.caffeine.CaffeineCache
+import scalacache.{Async => _, _}
 
-final class HttpExistenceClient[F[_]](
+final class HttpExistenceClient[F[_]](statusCache: Cache[Status])(
     implicit
     client: Client[F],
     logger: Logger[F],
+    mode: Mode[F],
     F: MonadThrowable[F]
 ) {
   def exists(uri: String): F[Boolean] = F.fromEither(Uri.fromString(uri)).flatMap(exists)
 
-  def exists(uri: Uri): F[Boolean] = {
-    val req = Request[F](method = Method.HEAD, uri = uri)
-    client.status(req).map(_ === Status.Ok).handleErrorWith { throwable =>
+  def exists(uri: Uri): F[Boolean] =
+    status(uri).map(_ === Status.Ok).handleErrorWith { throwable =>
       logger.debug(throwable)(s"Failed to check if $uri exists").as(false)
     }
+
+  private def status(uri: Uri): F[Status] =
+    statusCache.cachingForMemoizeF(uri.renderString)(None) {
+      client.status(Request[F](method = Method.HEAD, uri = uri))
+    }
+}
+
+object HttpExistenceClient {
+  def create[F[_]](
+      implicit
+      client: Client[F],
+      logger: Logger[F],
+      F: Async[F]
+  ): Resource[F, HttpExistenceClient[F]] = {
+    val buildCache = F.delay {
+      CaffeineCache(Caffeine.newBuilder().maximumSize(16384L).build[String, Entry[Status]]())
+    }
+    Resource.make(buildCache)(_.close().void).map(new HttpExistenceClient[F](_))
   }
 }
