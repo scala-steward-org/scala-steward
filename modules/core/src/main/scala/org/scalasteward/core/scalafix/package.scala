f313456522ef370bf64f13b0eb995b9a14279a6c
Filipe Regadas <filiperegadas@gmail.com>
2020-01-13 10:12:59 -0500

    fixup! Add new scio 0.8.0 scalafix rule

--- modules/core/src/main/scala/org/scalasteward/core/scalafix/package.scala
+++ modules/core/src/main/scala/org/scalasteward/core/scalafix/package.scala
@@ -32,14 +32,19 @@ package object scalafix {
       Migration(
         GroupId("com.spotify"),
         Nel.of("scio-.*"),
-        Version("0.8.0"),
+        Version("0.7.0"),
         Nel.of(
           "github:spotify/scio/FixAvroIO?sha=v0.7.4",
           "github:spotify/scio/AddMissingImports?sha=v0.7.4",
           "github:spotify/scio/RewriteSysProp?sha=v0.7.4",
-          "github:spotify/scio/BQClientRefactoring?sha=v0.7.4",
-          "github:spotify/scio/MigrateV0_8?sha=v0.8.0"
+          "github:spotify/scio/BQClientRefactoring?sha=v0.7.4"
         )
+      ),
+      Migration(
+        GroupId("com.spotify"),
+        Nel.of("scio-.*"),
+        Version("0.8.0"),
+        Nel.of("github:spotify/scio/MigrateV0_8?sha=v0.8.0")
       ),
       Migration(
         GroupId("org.http4s"),
