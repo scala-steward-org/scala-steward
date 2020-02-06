383399bc61a7935ca57cb4328d78fe48daeea408
Bayram Kiran <kiranbayram@gmail.com>
2020-02-06 15:44:36 +0100

    Fetch tags in GitAlg.syncFork (#1293)

--- modules/core/src/test/scala/org/scalasteward/core/git/GitAlgTest.scala
+++ modules/core/src/test/scala/org/scalasteward/core/git/GitAlgTest.scala
@@ -89,7 +89,7 @@ class GitAlgTest extends AnyFunSuite with Matchers {
           "upstream",
           "http://github.com/fthomas/datapackage"
         ),
-        List(askPass, repoDir, "git", "fetch", "upstream", "master"),
+        List(askPass, repoDir, "git", "fetch", "--tags", "upstream", "master"),
         List(askPass, repoDir, "git", "checkout", "-B", "master", "--track", "upstream/master"),
         List(askPass, repoDir, "git", "merge", "upstream/master"),
         List(askPass, repoDir, "git", "push", "--force", "--set-upstream", "origin", "master")
