5415be26215ecb4ef3ac55291e5d890e3a068b6f
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-02-08 02:29:19 +0100

    Update circe-generic, circe-literal, ... to 0.13.0 (#1300)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val catsEffect = "org.typelevel" %% "cats-effect" % "2.1.1"
   val catsKernelLaws = "org.typelevel" %% "cats-kernel-laws" % "2.1.0"
   val circeConfig = "io.circe" %% "circe-config" % "0.7.0"
-  val circeGeneric = "io.circe" %% "circe-generic" % "0.13.0-RC1"
+  val circeGeneric = "io.circe" %% "circe-generic" % "0.13.0"
   val circeGenericExtras = "io.circe" %% "circe-generic-extras" % "0.12.2"
   val circeLiteral = "io.circe" %% "circe-literal" % circeGeneric.revision
   val circeParser = "io.circe" %% "circe-parser" % circeGeneric.revision
