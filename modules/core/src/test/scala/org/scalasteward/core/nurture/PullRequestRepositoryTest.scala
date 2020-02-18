a13c916bfed826a159f026498e4783008e8b0fd2
Frank S. Thomas <frank@timepit.eu>
2020-02-18 22:38:11 +0100

    test

--- modules/core/src/test/scala/org/scalasteward/core/nurture/PullRequestRepositoryTest.scala
+++ modules/core/src/test/scala/org/scalasteward/core/nurture/PullRequestRepositoryTest.scala
@@ -13,7 +13,7 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 class PullRequestRepositoryTest extends AnyFunSuite with Matchers {
-  test("createOrUpdate >> findPullRequest") {
+  test("createOrUpdate >> findPullRequest >> lastPullRequestCreatedAt") {
     val repo = Repo("typelevel", "cats")
     val url = uri"https://github.com/typelevel/cats/pull/3291"
     val sha1 = Sha1(HexString("a2ced5793c2832ada8c14ba5c77e51c4bc9656a8"))
@@ -23,15 +23,18 @@ class PullRequestRepositoryTest extends AnyFunSuite with Matchers {
     val p = for {
       _ <- pullRequestRepository.createOrUpdate(repo, url, sha1, update, PullRequestState.Open)
       result <- pullRequestRepository.findPullRequest(repo, update.crossDependency, "1.0.0")
-    } yield result
-    val (state, result) = p.run(MockState.empty).unsafeRunSync()
+      createdAt <- pullRequestRepository.lastPullRequestCreatedAt(repo)
+    } yield (result, createdAt)
+    val (state, (result, createdAt)) = p.run(MockState.empty).unsafeRunSync()
 
     val store = (config.workspace / "store/pull_requests/v1/typelevel/cats/pull_requests.json")
     result shouldBe Some((url, sha1, PullRequestState.Open))
+    createdAt.isDefined shouldBe true
     state.copy(files = Map.empty) shouldBe MockState.empty.copy(
       commands = Vector(
         List("read", store.toString),
         List("write", store.toString),
+        List("read", store.toString),
         List("read", store.toString)
       )
     )
