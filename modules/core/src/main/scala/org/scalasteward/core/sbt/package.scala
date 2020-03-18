b207b6e9a0a791f15f3fe396cb2a95afe220b2e0
Scala Steward <me@scala-steward.org>
2020-03-18 18:19:54 +0100

    Update sbt-scalafix to 0.9.12

--- modules/core/src/main/scala/org/scalasteward/core/sbt/package.scala
+++ modules/core/src/main/scala/org/scalasteward/core/sbt/package.scala
@@ -41,7 +41,7 @@ package object sbt {
   val scalaStewardScalafixSbt: FileData =
     FileData(
       "scala-steward-scalafix.sbt",
-      """addSbtPlugin("ch.epfl.scala" % "sbt-scalafix" % "0.9.11")"""
+      """addSbtPlugin("ch.epfl.scala" % "sbt-scalafix" % "0.9.12")"""
     )
 
   def stewardPlugin[F[_]](implicit fileAlg: FileAlg[F], F: Functor[F]): F[FileData] = {
