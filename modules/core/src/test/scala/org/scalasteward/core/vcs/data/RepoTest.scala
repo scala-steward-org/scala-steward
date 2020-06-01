2b491e480ba3bc8d8b8b01cea6f586f8ad16a1bd
Paweł Kiersznowski <pkiersznowski@iterato.rs>
2020-06-02 01:00:11 +0200

    adding more asserts for decoding repos

--- modules/core/src/test/scala/org/scalasteward/core/vcs/data/RepoTest.scala
+++ modules/core/src/test/scala/org/scalasteward/core/vcs/data/RepoTest.scala
@@ -1,15 +1,18 @@
 package org.scalasteward.core.vcs.data
 
+import org.scalasteward.core.git.Branch
 import org.scalasteward.core.vcs.data.Repo.repoKeyDecoder
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 class RepoTest extends AnyFunSuite with Matchers {
   test("decode") {
-    repoKeyDecoder("owner/repo") shouldBe Some(Repo("owner", "repo"))
+    repoKeyDecoder("owner/repo") shouldBe Some(Repo("owner", "repo", None))
+    repoKeyDecoder("owner/repo#branch") shouldBe Some(Repo("owner", "repo", Some(Branch("branch"))))
   }
 
   test("decode sub group") {
-    repoKeyDecoder("group1/group2/project1") shouldBe Some(Repo("group1/group2", "project1"))
+    repoKeyDecoder("group1/group2/project1") shouldBe Some(Repo("group1/group2", "project1", None))
+    repoKeyDecoder("group1/group2/project1#branch") shouldBe Some(Repo("group1/group2", "project1", Some(Branch("branch"))))
   }
 }
