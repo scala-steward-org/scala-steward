2dbb90f3012cd0e95e72a42039bf07a8042e96dc
Paweł Kiersznowski <pkiersznowski@iterato.rs>
2020-06-05 00:08:21 +0200

    adjusting tests for checking branch names

--- modules/core/src/test/scala/org/scalasteward/core/vcs/VCSPackageTest.scala
+++ modules/core/src/test/scala/org/scalasteward/core/vcs/VCSPackageTest.scala
@@ -4,24 +4,25 @@ import org.http4s.syntax.literals._
 import org.scalasteward.core.TestSyntax._
 import org.scalasteward.core.application.SupportedVCS.{GitHub, Gitlab}
 import org.scalasteward.core.data.Update
+import org.scalasteward.core.git.Branch
 import org.scalasteward.core.util.Nel
 import org.scalasteward.core.vcs.data.Repo
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 class VCSPackageTest extends AnyFunSuite with Matchers {
-  val repo: Repo = Repo("foo", "bar")
+  val repo: Repo = Repo("foo", "bar", None)
   val update: Update.Single =
     Update.Single("ch.qos.logback" % "logback-classic" % "1.2.0", Nel.of("1.2.3"))
 
   test("listingBranch") {
-    listingBranch(GitHub, repo, update) shouldBe "foo/bar:update/logback-classic-1.2.3"
-    listingBranch(Gitlab, repo, update) shouldBe "update/logback-classic-1.2.3"
+    listingBranch(GitHub, repo, update, Branch("master")) shouldBe "foo/bar:master#update/logback-classic-1.2.3"
+    listingBranch(Gitlab, repo, update, Branch("master")) shouldBe "master#update/logback-classic-1.2.3"
   }
 
   test("createBranch") {
-    createBranch(GitHub, repo, update) shouldBe "foo:update/logback-classic-1.2.3"
-    createBranch(Gitlab, repo, update) shouldBe "update/logback-classic-1.2.3"
+    createBranch(GitHub, repo, update, Branch("master")) shouldBe "foo:master#update/logback-classic-1.2.3"
+    createBranch(Gitlab, repo, update, Branch("master")) shouldBe "master#update/logback-classic-1.2.3"
   }
 
   test("possibleCompareUrls") {
