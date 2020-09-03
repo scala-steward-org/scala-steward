b777d3a472f963c674a4e01183992943436f676b
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-09-03 14:51:22 +0200

    Update mill-scalalib to 0.8.0-8-0a8d46 (#1597)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt.Keys._
 
 object Dependencies {
   val mill = Def.setting {
-    val version = if (scalaBinaryVersion.value == "2.12") "0.6.3" else "0.8.0-6-bbbf19"
+    val version = if (scalaBinaryVersion.value == "2.12") "0.6.3" else "0.8.0-8-0a8d46"
     "com.lihaoyi" %% "mill-scalalib" % version
   }
   val attoCore = "org.tpolecat" %% "atto-core" % "0.8.0"
