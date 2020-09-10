b22378c3f1cef47a39fe1312a505aa9ff6ec1d64
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-09-10 17:13:13 +0200

    Update mill-scalalib to 0.8.0-11-8cd135 (#1611)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import sbt.librarymanagement.syntax.ExclusionRule
 
 object Dependencies {
   val mill = Def.setting {
-    val version = if (scalaBinaryVersion.value == "2.12") "0.6.3" else "0.8.0-10-096f6f"
+    val version = if (scalaBinaryVersion.value == "2.12") "0.6.3" else "0.8.0-11-8cd135"
     "com.lihaoyi" %% "mill-scalalib" % version
   }
   val attoCore = "org.tpolecat" %% "atto-core" % "0.8.0"
