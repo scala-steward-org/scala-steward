6c503e9531b16274c8192e5f64ca1e63bf7a4a30
Frank S. Thomas <frank@timepit.eu>
2020-09-13 09:30:12 +0200

    Check available disk space before running

--- scripts/run.sh
+++ scripts/run.sh
@@ -10,6 +10,9 @@ git pull
 sbt -no-colors ";clean ;core/assembly"
 JAR=$(find modules/ -name "scala-steward-assembly*.jar" | head -n1)
 
+# Don't start if there is not enough available disk space.
+test `df --output=avail / | tail -n1` -gt 102400
+
 # Don't start if we can't reach Maven Central.
 curl -s --head --fail https://repo1.maven.org/maven2/
 
