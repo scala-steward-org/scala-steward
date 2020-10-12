77aefe374ecb1f89cc89bfa558e9aa65e7c2f646
Frank S. Thomas <frank@timepit.eu>
2020-10-12 17:07:49 +0200

    Run mill with execSandboxed
    We're doing the same with sbt and Maven. All builds should be run with
    `execSandboxed` since we're executing user-provided code.

--- modules/core/src/main/scala/org/scalasteward/core/buildtool/mill/MillAlg.scala
+++ modules/core/src/main/scala/org/scalasteward/core/buildtool/mill/MillAlg.scala
@@ -17,14 +17,13 @@
 package org.scalasteward.core.buildtool.mill
 
 import cats.syntax.all._
-import cats.effect.Sync
 import org.scalasteward.core.BuildInfo
 import org.scalasteward.core.buildtool.BuildToolAlg
 import org.scalasteward.core.data.Scope
 import org.scalasteward.core.data.Scope.Dependencies
 import org.scalasteward.core.io.{FileAlg, ProcessAlg, WorkspaceAlg}
 import org.scalasteward.core.scalafix.Migration
-import org.scalasteward.core.util.Nel
+import org.scalasteward.core.util.{MonadThrow, Nel}
 import org.scalasteward.core.vcs.data.Repo
 
 trait MillAlg[F[_]] extends BuildToolAlg[F]
@@ -43,7 +42,7 @@ object MillAlg {
       fileAlg: FileAlg[F],
       processAlg: ProcessAlg[F],
       workspaceAlg: WorkspaceAlg[F],
-      F: Sync[F]
+      F: MonadThrow[F]
   ): MillAlg[F] =
     new MillAlg[F] {
       override def containsBuild(repo: Repo): F[Boolean] =
@@ -54,24 +53,15 @@ object MillAlg {
           repoDir <- workspaceAlg.repoDir(repo)
           predef = repoDir / "scala-steward.sc"
           _ <- fileAlg.writeFile(predef, content)
-          extracted <- processAlg.exec(
-            Nel(
-              "mill",
-              List(
-                "-i",
-                "-p",
-                predef.toString(),
-                "show",
-                s"${BuildInfo.millPluginModuleRootPkg}.StewardPlugin/extractDeps"
-              )
-            ),
+          extractDeps = s"${BuildInfo.millPluginModuleRootPkg}.StewardPlugin/extractDeps"
+          extracted <- processAlg.execSandboxed(
+            Nel("mill", List("-i", "-p", predef.toString(), "show", extractDeps)),
             repoDir
           )
           parsed <- F.fromEither(
             parser.parseModules(extracted.dropWhile(!_.startsWith("{")).mkString("\n"))
           )
           _ <- fileAlg.deleteForce(predef)
-
         } yield parsed.map(module => Scope(module.dependencies, module.repositories))
 
       override def runMigrations(repo: Repo, migrations: Nel[Migration]): F[Unit] = F.unit
