a1af971c55e78e8acc6cb6dac14c7b1af2743c68
Scala Steward <me@scala-steward.org>
2020-11-13 00:45:53 +0100

    Update discipline-scalatest to 2.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
   val coursierCore = "io.get-coursier" %% "coursier" % "2.0.6"
   val coursierCatsInterop = "io.get-coursier" %% "coursier-cats-interop" % coursierCore.revision
   val cron4sCore = "com.github.alonsodomin.cron4s" %% "cron4s-core" % "0.6.1"
-  val disciplineScalatest = ("org.typelevel" %% "discipline-scalatest" % "2.0.1")
+  val disciplineScalatest = ("org.typelevel" %% "discipline-scalatest" % "2.1.0")
     .excludeAll(ExclusionRule().withOrganization("org.scalatest"))
   val fs2Core = "co.fs2" %% "fs2-core" % "2.4.5"
   val fs2Io = "co.fs2" %% "fs2-io" % fs2Core.revision
