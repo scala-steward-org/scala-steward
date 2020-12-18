d509b95f4ff9fa00329a56e7787a5bdb1930fe97
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-12-18 18:10:43 +0100

    Update cats-core, cats-laws to 2.3.1 (#1815)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   val betterMonadicFor = "com.olegpy" %% "better-monadic-for" % "0.3.1"
   val caseApp = "com.github.alexarchambault" %% "case-app" % "2.0.4"
   val catsEffect = "org.typelevel" %% "cats-effect" % "2.3.0"
-  val catsCore = "org.typelevel" %% "cats-core" % "2.3.0"
+  val catsCore = "org.typelevel" %% "cats-core" % "2.3.1"
   val catsLaws = "org.typelevel" %% "cats-laws" % catsCore.revision
   val circeConfig = "io.circe" %% "circe-config" % "0.8.0"
   val circeGeneric = "io.circe" %% "circe-generic" % "0.13.0"
