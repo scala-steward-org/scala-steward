c3f8b5f96fd983df08d7d6a489201555cb2e8507
Frank S. Thomas <frank@timepit.eu>
2020-12-31 16:55:41 +0100

    Clean up repoconfig tests

--- modules/core/src/test/scala/org/scalasteward/core/repoconfig/PullRequestsConfigTest.scala
+++ modules/core/src/test/scala/org/scalasteward/core/repoconfig/PullRequestsConfigTest.scala
@@ -2,17 +2,8 @@ package org.scalasteward.core.repoconfig
 
 import cats.kernel.laws.discipline.SemigroupTests
 import munit.DisciplineSuite
-import org.scalacheck.{Arbitrary, Gen}
-import org.scalasteward.core.repoconfig.PullRequestFrequency.{Asap, Timespan}
-import scala.concurrent.duration._
+import org.scalasteward.core.TestInstances._
 
 class PullRequestsConfigTest extends DisciplineSuite {
-  implicit val pullRequestFrequencyArbitrary: Arbitrary[PullRequestFrequency] =
-    Arbitrary(Arbitrary.arbitrary[FiniteDuration].flatMap(fd => Gen.oneOf(Asap, Timespan(fd))))
-
-  implicit val pullRequestsConfigArbitrary: Arbitrary[PullRequestsConfig] = Arbitrary(for {
-    e <- Arbitrary.arbitrary[Option[PullRequestFrequency]]
-  } yield PullRequestsConfig(e))
-
   checkAll("Semigroup[PullRequestsConfig]", SemigroupTests[PullRequestsConfig].semigroup)
 }
--- modules/core/src/test/scala/org/scalasteward/core/repoconfig/ScalafmtConfigTest.scala
+++ modules/core/src/test/scala/org/scalasteward/core/repoconfig/ScalafmtConfigTest.scala
@@ -4,6 +4,6 @@ import cats.kernel.laws.discipline.SemigroupTests
 import munit.DisciplineSuite
 import org.scalasteward.core.TestInstances._
 
-class CommitsConfigTest extends DisciplineSuite {
-  checkAll("Semigroup[CommitsConfig]", SemigroupTests[CommitsConfig].semigroup)
+class ScalafmtConfigTest extends DisciplineSuite {
+  checkAll("Semigroup[ScalafmtConfig]", SemigroupTests[ScalafmtConfig].semigroup)
 }
