c985930f50963dc6bcb2ac4cb84db9f15e93492a
Scala Steward <me@scala-steward.org>
2021-02-16 20:57:38 +0100

    Update scalacheck to 1.15.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -46,5 +46,5 @@ object Dependencies {
   val scalacacheCaffeine = "com.github.cb372" %% "scalacache-caffeine" % "0.28.0"
   val scalacacheCatsEffect =
     "com.github.cb372" %% "scalacache-cats-effect" % scalacacheCaffeine.revision
-  val scalacheck = "org.scalacheck" %% "scalacheck" % "1.15.2"
+  val scalacheck = "org.scalacheck" %% "scalacheck" % "1.15.3"
 }
