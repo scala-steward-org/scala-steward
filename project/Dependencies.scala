b5cb8d32cf8cda701ab0a2425b16cb7681cc2706
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-03-21 14:37:13 +0100

    Update coursier, coursier-cats-interop to 2.0.15 (#2020)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
   val circeParser = "io.circe" %% "circe-parser" % circeGeneric.revision
   val circeRefined = "io.circe" %% "circe-refined" % circeGeneric.revision
   val commonsIo = "commons-io" % "commons-io" % "2.8.0"
-  val coursierCore = "io.get-coursier" %% "coursier" % "2.0.14"
+  val coursierCore = "io.get-coursier" %% "coursier" % "2.0.15"
   val coursierCatsInterop = "io.get-coursier" %% "coursier-cats-interop" % coursierCore.revision
   val cron4sCore = "com.github.alonsodomin.cron4s" %% "cron4s-core" % "0.6.1"
   val disciplineMunit = "org.typelevel" %% "discipline-munit" % "1.0.6"
