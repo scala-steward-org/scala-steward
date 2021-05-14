9b1175210c902dbb63ea03d1a2136868ffb54f16
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-05-15 00:10:11 +0200

    Update atto-core to 0.9.5 (#2121)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt.Keys._
 import sbt.librarymanagement.syntax.ExclusionRule
 
 object Dependencies {
-  val attoCore = "org.tpolecat" %% "atto-core" % "0.9.4"
+  val attoCore = "org.tpolecat" %% "atto-core" % "0.9.5"
   val bcprovJdk15to18 = "org.bouncycastle" % "bcprov-jdk15to18" % "1.68"
   val betterFiles = "com.github.pathikrit" %% "better-files" % "3.9.1"
   val caseApp = "com.github.alexarchambault" %% "case-app" % "2.0.6"
