88024c7c3196dcdb1b55bbd1773e6bf09048b95e
Frank Thomas <frank@timepit.eu>
2022-02-05 11:41:35 +0100

    Reset update branch before merging the base branch (#2504)
    * Reset update branch before merging the base branch
    
    Closes #2503.
    
    * Write as for-comprehension
    
    * Use git revert instead of diff and apply
    
    * Guard against an empty list of commits
    
    * Introduce val for more clarity

--- modules/core/src/main/scala/org/scalasteward/core/nurture/NurtureAlg.scala
+++ modules/core/src/main/scala/org/scalasteward/core/nurture/NurtureAlg.scala
@@ -244,9 +244,11 @@ final class NurtureAlg[F[_]](config: VCSCfg)(implicit
       _ <- logger.info(
         s"Merge branch ${data.baseBranch.name} into ${data.updateBranch.name} and apply again"
       )
+      maybeRevertCommit <- gitAlg.revertChanges(data.repo, data.baseBranch)
       maybeMergeCommit <- gitAlg.mergeTheirs(data.repo, data.baseBranch)
       edits <- editAlg.applyUpdate(data.repoData, data.update)
       editCommits = edits.flatMap(_.maybeCommit)
-      result <- pushCommits(data, maybeMergeCommit.toList ++ editCommits)
+      commits = maybeRevertCommit.toList ++ maybeMergeCommit.toList ++ editCommits
+      result <- pushCommits(data, commits)
     } yield result
 }
