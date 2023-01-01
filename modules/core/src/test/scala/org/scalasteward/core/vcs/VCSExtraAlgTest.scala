abf2368b0873f2d592794cd86d264782164af3a2
Frank S. Thomas <frank@timepit.eu>
2023-01-01 22:18:12 +0100

    Use CatsEffectSuite

--- modules/core/src/test/scala/org/scalasteward/core/vcs/VCSExtraAlgTest.scala
+++ modules/core/src/test/scala/org/scalasteward/core/vcs/VCSExtraAlgTest.scala
@@ -1,8 +1,7 @@
 package org.scalasteward.core.vcs
 
 import cats.effect.IO
-import cats.effect.unsafe.implicits.global
-import munit.FunSuite
+import munit.CatsEffectSuite
 import org.http4s.HttpApp
 import org.http4s.dsl.io._
 import org.http4s.implicits._
@@ -12,7 +11,7 @@ import org.scalasteward.core.data.Version
 import org.scalasteward.core.mock.MockContext.context._
 import org.scalasteward.core.mock.{MockEff, MockState}
 
-class VCSExtraAlgTest extends FunSuite {
+class VCSExtraAlgTest extends CatsEffectSuite {
   private val state = MockState.empty.copy(clientResponses = HttpApp[IO] {
     case HEAD -> Root / "foo" / "bar" / "compare" / "v0.1.0...v0.2.0" => Ok("exist")
     case HEAD -> Root / "foo" / "buz" / "compare" / "v0.1.0...v0.2.0" => PermanentRedirect()
@@ -23,28 +22,22 @@ class VCSExtraAlgTest extends FunSuite {
   private val v2 = Version("0.2.0")
 
   test("getReleaseRelatedUrls: repoUrl not found") {
-    val obtained = vcsExtraAlg
-      .getReleaseRelatedUrls(uri"https://github.com/foo/foo", v1, v2)
-      .runA(state)
-      .unsafeRunSync()
-    assertEquals(obtained, List.empty)
+    val obtained =
+      vcsExtraAlg.getReleaseRelatedUrls(uri"https://github.com/foo/foo", v1, v2).runA(state)
+    assertIO(obtained, List.empty)
   }
 
   test("getReleaseRelatedUrls: repoUrl ok") {
-    val obtained = vcsExtraAlg
-      .getReleaseRelatedUrls(uri"https://github.com/foo/bar", v1, v2)
-      .runA(state)
-      .unsafeRunSync()
+    val obtained =
+      vcsExtraAlg.getReleaseRelatedUrls(uri"https://github.com/foo/bar", v1, v2).runA(state)
     val expected = List(VersionDiff(uri"https://github.com/foo/bar/compare/v0.1.0...v0.2.0"))
-    assertEquals(obtained, expected)
+    assertIO(obtained, expected)
   }
 
   test("getReleaseRelatedUrls: repoUrl permanent redirect") {
-    val obtained = vcsExtraAlg
-      .getReleaseRelatedUrls(uri"https://github.com/foo/buz", v1, v2)
-      .runA(state)
-      .unsafeRunSync()
-    assertEquals(obtained, List.empty)
+    val obtained =
+      vcsExtraAlg.getReleaseRelatedUrls(uri"https://github.com/foo/buz", v1, v2).runA(state)
+    assertIO(obtained, List.empty)
   }
 
   private val config = VCSCfg(
@@ -60,25 +53,22 @@ class VCSExtraAlgTest extends FunSuite {
     val obtained = githubOnPremVcsExtraAlg
       .getReleaseRelatedUrls(uri"https://github.on-prem.com/foo/foo", v1, v2)
       .runA(state)
-      .unsafeRunSync()
-    assertEquals(obtained, List.empty)
+    assertIO(obtained, List.empty)
   }
 
   test("getReleaseRelatedUrls: on-prem, repoUrl ok") {
     val obtained = githubOnPremVcsExtraAlg
       .getReleaseRelatedUrls(uri"https://github.on-prem.com/foo/bar", v1, v2)
       .runA(state)
-      .unsafeRunSync()
     val expected =
       List(VersionDiff(uri"https://github.on-prem.com/foo/bar/compare/v0.1.0...v0.2.0"))
-    assertEquals(obtained, expected)
+    assertIO(obtained, expected)
   }
 
   test("getReleaseRelatedUrls: on-prem, repoUrl permanent redirect") {
     val obtained = githubOnPremVcsExtraAlg
       .getReleaseRelatedUrls(uri"https://github.on-prem.com/foo/buz", v1, v2)
       .runA(state)
-      .unsafeRunSync()
-    assertEquals(obtained, List.empty)
+    assertIO(obtained, List.empty)
   }
 }
