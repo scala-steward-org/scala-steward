39690598c6d82ddf4457abb58cc62ac5756b11f4
Scala Steward <scala_steward@virtuslab.com>
2023-01-03 18:43:33 +0000

    Update monocle-core to 3.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -33,7 +33,7 @@ object Dependencies {
   val jjwtJackson = "io.jsonwebtoken" % "jjwt-jackson" % jjwtApi.revision
   val millVersion = "0.10.10"
   val millMain = "com.lihaoyi" % "mill-main_2.13" % millVersion
-  val monocleCore = "dev.optics" %% "monocle-core" % "3.1.0"
+  val monocleCore = "dev.optics" %% "monocle-core" % "3.2.0"
   val munit = "org.scalameta" %% "munit" % "0.7.29"
   val munitCatsEffect = "org.typelevel" %% "munit-cats-effect-3" % "1.0.7"
   val munitScalacheck = "org.scalameta" %% "munit-scalacheck" % munit.revision
