227669af5c946c91f4cd8aa850fa346bdf105f6d
Frank S. Thomas <frank@timepit.eu>
2023-01-18 15:05:41 +0100

    Extract dependencies from scala-cli via sbt export

---
+++ modules/core/src/main/scala/org/scalasteward/core/buildtool/scalacli/ScalaCliAlg.scala
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2018-2023 Scala Steward contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.scalasteward.core.buildtool.scalacli
+
+import cats.Monad
+import cats.syntax.all._
+import org.scalasteward.core.buildtool.sbt.SbtAlg
+import org.scalasteward.core.buildtool.{BuildRoot, BuildToolAlg}
+import org.scalasteward.core.data.Scope.Dependencies
+import org.scalasteward.core.edit.scalafix.ScalafixMigration
+import org.scalasteward.core.git.GitAlg
+import org.scalasteward.core.io.{ProcessAlg, WorkspaceAlg}
+import org.scalasteward.core.util.Nel
+
+final class ScalaCliAlg[F[_]](implicit
+    gitAlg: GitAlg[F],
+    processAlg: ProcessAlg[F],
+    sbtAlg: SbtAlg[F],
+    workspaceAlg: WorkspaceAlg[F],
+    F: Monad[F]
+) extends BuildToolAlg[F] {
+  override def name: String = "Scala CLI"
+
+  override def containsBuild(buildRoot: BuildRoot): F[Boolean] =
+    gitAlg.findFilesContaining(buildRoot.repo, "//> using lib").map(_.nonEmpty)
+
+  override def getDependencies(buildRoot: BuildRoot): F[List[Dependencies]] =
+    for {
+      buildRootDir <- workspaceAlg.buildRootDir(buildRoot)
+      exportDir = "tmp-sbt-build-for-scala-steward"
+      exportCmd = Nel.of(
+        "scala-cli",
+        "export",
+        "--sbt",
+        "--output",
+        exportDir,
+        buildRootDir.pathAsString
+      )
+      _ <- processAlg.execSandboxed(exportCmd, buildRootDir)
+      exportBuildRoot = buildRoot.copy(relativePath = buildRoot.relativePath + s"/$exportDir")
+      dependencies <- sbtAlg.getDependencies(exportBuildRoot)
+    } yield dependencies
+
+  override def runMigration(buildRoot: BuildRoot, migration: ScalafixMigration): F[Unit] =
+    F.unit
+}
