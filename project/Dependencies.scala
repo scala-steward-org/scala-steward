ca6c93e44ab9e44fa25cb5117c2868072d2d2f4c
Scala Steward <me@scala-steward.org>
2023-02-09 16:55:34 +0100

    Update fs2-core, fs2-io to 3.6.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
   val cron4sCore = "com.github.alonsodomin.cron4s" %% "cron4s-core" % "0.6.1"
   val decline = "com.monovore" %% "decline" % "2.4.1"
   val disciplineMunit = "org.typelevel" %% "discipline-munit" % "1.0.9"
-  val fs2Core = "co.fs2" %% "fs2-core" % "3.6.0"
+  val fs2Core = "co.fs2" %% "fs2-core" % "3.6.1"
   val fs2Io = "co.fs2" %% "fs2-io" % fs2Core.revision
   val http4sCore = "org.http4s" %% "http4s-core" % "1.0.0-M39"
   val http4sCirce = "org.http4s" %% "http4s-circe" % http4sCore.revision
