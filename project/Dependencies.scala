980683d94615fe9db468172e5c4d04c6fd3dd27f
Scala Steward <scala_steward@virtuslab.com>
2024-04-19 20:24:29 +0000

    Update bcprov-jdk15to18 to 1.78.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  val bcprovJdk15to18 = "org.bouncycastle" % "bcprov-jdk15to18" % "1.78"
+  val bcprovJdk15to18 = "org.bouncycastle" % "bcprov-jdk15to18" % "1.78.1"
   val betterFiles = "com.github.pathikrit" %% "better-files" % "3.9.2"
   val catsEffect = "org.typelevel" %% "cats-effect" % "3.5.4"
   val catsCore = "org.typelevel" %% "cats-core" % "2.10.0"
