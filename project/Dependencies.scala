de0253af556d9dabdfef39e1287f16e5efa49b2e
Scala Steward <scala_steward@virtuslab.com>
2024-05-23 19:47:43 +0000

    Update discipline-munit to 2.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
     "io.get-coursier" %% "coursier-sbt-maven-repository" % coursierCore.revision
   val cron4sCore = "com.github.alonsodomin.cron4s" %% "cron4s-core" % "0.7.0"
   val decline = "com.monovore" %% "decline" % "2.4.1"
-  val disciplineMunit = "org.typelevel" %% "discipline-munit" % "1.0.9"
+  val disciplineMunit = "org.typelevel" %% "discipline-munit" % "2.0.0"
   val fs2Core = "co.fs2" %% "fs2-core" % "3.10.2"
   val fs2Io = "co.fs2" %% "fs2-io" % fs2Core.revision
   val http4sCore = "org.http4s" %% "http4s-core" % "1.0.0-M41"
