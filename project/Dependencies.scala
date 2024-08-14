4e9e0f72a6211de2519c7e60d252bc22623a7a78
Scala Steward <scala_steward@virtuslab.com>
2024-08-14 02:46:27 +0000

    Update munit to 1.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -35,7 +35,7 @@ object Dependencies {
   val jjwtJackson = "io.jsonwebtoken" % "jjwt-jackson" % jjwtApi.revision
   val millScriptVersion = "0.11.0-M10"
   val monocleCore = "dev.optics" %% "monocle-core" % "3.3.0"
-  val munit = "org.scalameta" %% "munit" % "1.0.0"
+  val munit = "org.scalameta" %% "munit" % "1.0.1"
   val munitCatsEffect = "org.typelevel" %% "munit-cats-effect" % "2.0.0"
   val munitScalacheck = "org.scalameta" %% "munit-scalacheck" % munit.revision
   val refined = "eu.timepit" %% "refined" % "0.11.2"
