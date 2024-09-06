3990d2e79f6ee83467f191e3bdfa93f28e81cfb2
Scala Steward <scala_steward@virtuslab.com>
2024-09-06 18:04:05 +0000

    Update sbt-scoverage to 2.2.0

--- modules/core/src/test/scala/org/scalasteward/core/edit/update/VersionPositionScannerTest.scala
+++ modules/core/src/test/scala/org/scalasteward/core/edit/update/VersionPositionScannerTest.scala
@@ -69,7 +69,7 @@ class VersionPositionScannerTest extends FunSuite {
       "plugins.sbt",
       s"""addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.5.2")
          |addSbtPlugin("${d.groupId}" % "${d.artifactId.name}" % "${d.version}")
-         |addSbtPlugin("org.scoverage" % "sbt-scoverage" % "2.1.1")""".stripMargin
+         |addSbtPlugin("org.scoverage" % "sbt-scoverage" % "2.2.0")""".stripMargin
     )
     val obtained = VersionPositionScanner.findPositions(d.version, fd)
     val expected = List(
