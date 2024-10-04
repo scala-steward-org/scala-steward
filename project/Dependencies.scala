01d5ce2d7bfe70829d201cdb5c298628d3771c1e
Scala Steward <scala_steward@virtuslab.com>
2024-10-04 18:15:44 +0000

    Update http4s-circe, http4s-client, ... to 1.0.0-M42

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
   val disciplineMunit = "org.typelevel" %% "discipline-munit" % "2.0.0"
   val fs2Core = "co.fs2" %% "fs2-core" % "3.11.0"
   val fs2Io = "co.fs2" %% "fs2-io" % fs2Core.revision
-  val http4sCore = "org.http4s" %% "http4s-core" % "1.0.0-M41"
+  val http4sCore = "org.http4s" %% "http4s-core" % "1.0.0-M42"
   val http4sCirce = "org.http4s" %% "http4s-circe" % http4sCore.revision
   val http4sClient = "org.http4s" %% "http4s-client" % http4sCore.revision
   val http4sDsl = "org.http4s" %% "http4s-dsl" % http4sCore.revision
