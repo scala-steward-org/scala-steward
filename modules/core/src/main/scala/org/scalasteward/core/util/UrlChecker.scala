ae2fddebf2ff79126b192a532d4ff74155584db8
danicheg <esik.danny@gmail.com>
2025-02-16 23:07:48 +0300

    Take redirects into account in UrlChecker

--- modules/core/src/main/scala/org/scalasteward/core/util/UrlChecker.scala
+++ modules/core/src/main/scala/org/scalasteward/core/util/UrlChecker.scala
@@ -20,6 +20,7 @@ import cats.effect.Sync
 import cats.syntax.all.*
 import com.github.benmanes.caffeine.cache.Caffeine
 import org.http4s.client.Client
+import org.http4s.headers.Location
 import org.http4s.{Method, Request, Status, Uri}
 import org.scalasteward.core.application.Config
 import org.typelevel.log4cats.Logger
@@ -27,21 +28,43 @@ import scalacache.Entry
 import scalacache.caffeine.CaffeineCache
 
 trait UrlChecker[F[_]] {
-  def exists(url: Uri): F[Boolean]
+  def validate(url: Uri): F[UrlChecker.UrlValidationResult]
 }
 
 final case class UrlCheckerClient[F[_]](client: Client[F]) extends AnyVal
 
 object UrlChecker {
+
+  sealed abstract class UrlValidationResult extends Product with Serializable {
+    def exists: Boolean =
+      fold(exists = true, notExists = false, redirectTo = _ => false)
+
+    def notExists: Boolean =
+      fold(exists = false, notExists = true, redirectTo = _ => false)
+
+    def fold[A](exists: => A, notExists: => A, redirectTo: Uri => A): A =
+      this match {
+        case UrlValidationResult.Exists          => exists
+        case UrlValidationResult.NotExists       => notExists
+        case UrlValidationResult.RedirectTo(url) => redirectTo(url)
+      }
+  }
+
+  object UrlValidationResult {
+    case object Exists extends UrlValidationResult
+    case object NotExists extends UrlValidationResult
+    final case class RedirectTo(url: Uri) extends UrlValidationResult
+  }
+
   private def buildCache[F[_]](config: Config)(implicit
       F: Sync[F]
-  ): F[CaffeineCache[F, String, Status]] =
+  ): F[CaffeineCache[F, String, UrlValidationResult]] =
     F.delay {
       val cache = Caffeine
         .newBuilder()
         .maximumSize(16384L)
         .expireAfterWrite(config.cacheTtl.length, config.cacheTtl.unit)
-        .build[String, Entry[Status]]()
+        .build[String, Entry[UrlValidationResult]]()
       CaffeineCache(cache)
     }
 
@@ -52,15 +75,36 @@ object UrlChecker {
   ): F[UrlChecker[F]] =
     buildCache(config).map { statusCache =>
       new UrlChecker[F] {
-        override def exists(url: Uri): F[Boolean] =
-          status(url).map(_ === Status.Ok).handleErrorWith { throwable =>
-            logger.debug(throwable)(s"Failed to check if $url exists").as(false)
-          }
+        override def validate(url: Uri): F[UrlValidationResult] =
+          check(url).handleErrorWith(th =>
+            logger
+              .debug(th)(s"Failed to check if $url exists")
+              .as(UrlValidationResult.NotExists)
+          )
 
-        private def status(url: Uri): F[Status] =
+        private def check(url: Uri): F[UrlValidationResult] =
           statusCache.cachingF(url.renderString)(None) {
             val req = Request[F](method = Method.HEAD, uri = url)
-            modify(req).flatMap(urlCheckerClient.client.status)
+
+            modify(req).flatMap(r =>
+              urlCheckerClient.client.run(r).use {
+                case resp if resp.status === Status.Ok =>
+                  F.pure(UrlValidationResult.Exists)
+
+                case resp if resp.status === Status.MovedPermanently =>
+                  val uriMaybe =
+                    resp.headers
+                      .get[Location]
+                      .fold[UrlValidationResult](UrlValidationResult.NotExists)(h =>
+                        UrlValidationResult.RedirectTo(h.uri)
+                      )
+
+                  F.pure(uriMaybe)
+
+                case _ =>
+                  F.pure(UrlValidationResult.NotExists)
+              }
+            )
           }
       }
     }
