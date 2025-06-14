6a868df6a67fbc591839934e4a636538af8c3296
Scala Steward <scala_steward@virtuslab.com>
2025-06-14 03:21:31 +0000

    Update circe-config to 0.10.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val catsCore = "org.typelevel" %% "cats-core" % "2.13.0"
   val catsLaws = "org.typelevel" %% "cats-laws" % catsCore.revision
   val catsParse = "org.typelevel" %% "cats-parse" % "1.1.0"
-  val circeConfig = "io.circe" %% "circe-config" % "0.10.1"
+  val circeConfig = "io.circe" %% "circe-config" % "0.10.2"
   val circeGeneric = "io.circe" %% "circe-generic" % "0.14.13"
   val circeLiteral = "io.circe" %% "circe-literal" % circeGeneric.revision
   val circeParser = "io.circe" %% "circe-parser" % circeGeneric.revision
