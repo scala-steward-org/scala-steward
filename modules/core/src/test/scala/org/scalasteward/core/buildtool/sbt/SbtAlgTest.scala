944141916b4af595477a9856915802a2605659a2
xuwei-k <6b656e6a69@gmail.com>
2026-06-18 15:40:34 +0900

    Add `--server` option to sbt

--- modules/core/src/test/scala/org/scalasteward/core/buildtool/sbt/SbtAlgTest.scala
+++ modules/core/src/test/scala/org/scalasteward/core/buildtool/sbt/SbtAlgTest.scala
@@ -33,6 +33,7 @@ class SbtAlgTest extends FunSuite {
         Cmd.execSandboxed(
           repoDir,
           "sbt",
+          "--server",
           "-Dsbt.color=false",
           "-Dsbt.log.noformat=true",
           "-Dsbt.supershell=false",
@@ -65,6 +66,7 @@ class SbtAlgTest extends FunSuite {
         Cmd.execSandboxed(
           repoDir,
           "sbt",
+          "--server",
           "-Dsbt.color=false",
           "-Dsbt.log.noformat=true",
           "-Dsbt.supershell=false",
@@ -104,6 +106,7 @@ class SbtAlgTest extends FunSuite {
         Cmd.execSandboxed(
           repoDir,
           "sbt",
+          "--server",
           "-Dsbt.color=false",
           "-Dsbt.log.noformat=true",
           "-Dsbt.supershell=false",
@@ -144,6 +147,7 @@ class SbtAlgTest extends FunSuite {
         Cmd.execSandboxed(
           repoDir,
           "sbt",
+          "--server",
           "-Dsbt.color=false",
           "-Dsbt.log.noformat=true",
           "-Dsbt.supershell=false",
